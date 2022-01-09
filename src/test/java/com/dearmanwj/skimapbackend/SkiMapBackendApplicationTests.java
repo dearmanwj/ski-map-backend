1619137d07551d8a66de8682111982e254103652
William Dearman <williamdearman@hotmail.co.uk>
2022-01-09 13:32:09 +0100

    Add boiler plate from spring initializr

---
+++ src/test/java/com/dearmanwj/skimapbackend/SkiMapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dearmanwj.skimapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiMapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
